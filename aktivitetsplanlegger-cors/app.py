7b83ca8e6201079304494415a22a728e51e9839e
Åse Jortveit Sagebakken <aase.sagebakken@hotmail.com>
2021-10-24 21:08:10 +0200

    comments

--- aktivitetsplanlegger-cors/app.py
+++ aktivitetsplanlegger-cors/app.py
@@ -10,14 +10,13 @@ from flask import Flask, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-# allow cross-origin requests (CORS)
+# Allow cross-origin requests (CORS)
 cors = CORS()
 
-# secret key for session
+# Secret key for session
 app.secret_key = 'A213FB1557589757D5ACEED'
 
-
-# add headers to use sessions on cross-origin requests
+# Add headers to use sessions on cross-origin requests
 @app.after_request
 def add_header(response):
     response.headers['Access-Control-Allow-Origin'] = 'http://localhost:8080'
@@ -26,15 +25,18 @@ def add_header(response):
     response.headers['Access-Control-Allow-Headers'] = "Content-Type, Content-Length, Authorization"
     return response
 
+# Upload folder
+UPLOAD_FOLDER = '/path/to/the/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# get database
+# Get database
 def get_db():
     if not hasattr(g, "_database"):
         print("create connection")
         g._database = sqlite3.connect("database.db")
     return g._database
 
-# close database
+# Close the database at the end of the request
 @app.teardown_appcontext
 def teardown_db(error):
     """Closes the database at the end of the request."""
@@ -43,8 +45,17 @@ def teardown_db(error):
         print("close connection")
         db.close()
 
+"""
+# Check if there is a loggedin user
+@app.route("/userdata", methods=["GET"])
+def userdata():
+    if session.get("username", None):
+        user = get_user()
+        return user
+    return jsonify("")
+"""
 
-# check if login is valid
+# Check if login is valid
 def valid_login(username, password):
     """Checks if username-password combination is valid."""
     conn = get_db()
@@ -54,7 +65,7 @@ def valid_login(username, password):
     return False
 
 
-# login
+# Login funksjon som sjekker om det er en valid login og setter en session på brukernavn og rollen til brukeren
 @app.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
@@ -66,8 +77,27 @@ def login():
     session["userid"] = user["userid"]
     return user
 
+"""
+@app.route("/login", methods=["GET", "POST"])
+def login():
+# if the form was submitted (otherwise we just display form)
+    if request.method == "POST":  
+        user_form = request.get_json()
+        username = user_form["username"]
+        password = user_form["password"]
+        if valid_login(username, password):
+            conn = get_db()
+            user = get_user_by_name(conn, username)
+            session["username"] = user["username"]
+            session["role"] = user["role"]
+            return jsonify(user)
+        else:
+            return jsonify("Feil brukernavn eller passord!")
+    return jsonify("")
+"""
 
-# logout
+
+# Logout
 @app.route("/logout")
 def logout():
     session.clear()
@@ -75,9 +105,16 @@ def logout():
     if "username" in session:
         del session["username"]
     return ""
+    
+"""
+@app.route("/logout", methods=["GET"])
+def logout():
+    session.pop("username", None)
+    session.pop("role", None)
+    return jsonify("")
+"""
 
-
-# register
+# Register
 @app.route("/signup", methods=["POST"])
 def signup_post():
     data = request.get_json()
@@ -91,7 +128,7 @@ def signup_post():
     return ""
 
 
-# get user
+# Get user
 @app.route("/user", methods=["GET"])
 def get_user(username):
     if userid == None:
@@ -101,7 +138,7 @@ def get_user(username):
     return user
 
 
-# get activities:
+# Get activities:
 @app.route("/activities_get", methods=["GET"])
 def get_activities():
     conn = get_db()
@@ -109,7 +146,7 @@ def get_activities():
     return json.dumps(activities)
 
 
-# add activity:
+# Add activity:
 @app.route("/add_activity_post", methods=["POST"])
 def add_activity_post():
     newactivity = request.get_json()
@@ -118,7 +155,7 @@ def add_activity_post():
     return ""
 
 
-# set activity:
+# Set activity:
 @app.route("/activities/<activity_id>", methods=["PUT"])
 def set_activity(activity_id):
     userid = session.get("userid", None)
@@ -134,7 +171,7 @@ def set_activity(activity_id):
     return ""
 
 
-# delete activity:
+# Delete activity:
 @app.route("/del_activities/<activity_id>", methods=["DELETE"])
 def del_activity(activity_id):
     print(activity_id)
